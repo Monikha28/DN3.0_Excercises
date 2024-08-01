70e7814064cc6603de9d26e4720188d1a82b9426
Monikha28 <monikhasrikanth@gmail.com>
2024-08-01 17:03:01 +0530

    Add files via upload

---
+++ Week1/Design Patterns/Factory Method Pattern/Document.java
@@ -0,0 +1,7 @@
+package factorymethodpatternexample;
+
+public abstract class Document {
+    public abstract void open();
+    public abstract void save();
+    public abstract void close();
+}
---
+++ Week1/Design Patterns/Factory Method Pattern/TestFactoryMethod.java
@@ -0,0 +1,26 @@
+package factorymethodpatternexample;
+
+public class TestFactoryMethod {
+    public static void main(String[] args) {
+        // Create a Word document
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+        wordDoc.save();
+        wordDoc.close();
+
+        // Create a PDF document
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+        pdfDoc.save();
+        pdfDoc.close();
+
+        // Create an Excel document
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+        excelDoc.save();
+        excelDoc.close();
+    }
+}
